714ee43fa60d9b6bc3e962ca35c4e2dfdfd2732a
Mohab <Mohab@localhost.localdomain>
2020-03-15 22:32:07 +0200

    deleteDB

---
+++ deleteDatabase.sh
@@ -0,0 +1,24 @@
+#!usr/bin/bash
+clear
+db=`ls ~/Databases | wc -l`
+
+if ((db == 0))
+then
+	echo "No DataBase To Delete"
+	exit
+else
+	ls ~/Databases
+	echo "Choose Database To Delete: "
+	read delete
+	if [ -d ~/Databases ]
+	then
+		rm -r ~/Databases/"$delete"
+		clear
+		echo "($delete) Deleted"
+		return
+	else
+		echo "No DataBase with this name"
+		exit
+	fi
+
+fi
