3f6cc60c091f8d7b56317b07889e8bb275fee941
Mohab <Mohab@localhost.localdomain>
2020-03-18 14:13:23 +0200

    Edit path

--- deleteDatabase.sh
+++ deleteDatabase.sh
@@ -1,18 +1,18 @@
 #!usr/bin/bash
 clear
-db=`ls ~/Databases | wc -l`
+db=`ls ./Databases | wc -l`
 
 if ((db == 0))
 then
 	echo "No DataBase To Delete"
 	exit
 else
-	ls ~/Databases
+	ls ./Databases
 	echo "Choose Database To Delete: "
 	read delete
-	if [ -d ~/Databases ]
+	if [ -d ./Databases ]
 	then
-		rm -r ~/Databases/"$delete"
+		rm -r ./Databases/"$delete"
 		clear
 		echo "($delete) Deleted"
 		return
